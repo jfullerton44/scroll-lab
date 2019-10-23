03cbb54fe53a9c41cbecaf063c4feaebc80de084
Jacob Fullerton <fullertonj44@gmail.com>
2019-10-22 23:10:20 -0400

    Day 1

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+/* eslint-disable require-jsdoc */
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Constants from 'expo-constants';
+import HomeScreen from './components/HomeScreen';
+import { Card } from 'react-native-paper';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native?!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <HomeScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ components/HomeScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Easing,Text, Image, Animated,TouchableOpacity, StyleSheet, View, ScrollView,TouchableHighlight } from 'react-native';
+import InviteCard from './InviteCard';
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <InviteCard id = "some ID"  name="fake name" date="fake date" />
+      </View>
+    )
+  }
+}
+
+
+export default HomeScreen;
